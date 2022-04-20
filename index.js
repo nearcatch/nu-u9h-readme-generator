5cd04d312023c76422f187e4c3bc0b958811f04a
nearcatch <692914+nearcatch@users.noreply.github.com>
2022-04-20 17:27:25 -0500

    refactor into functions

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-
-inquirer
-  .prompt([
+function infoPrompt() {
+  return inquirer.prompt([
     {
       type: "input",
       message: "What is the project title?",
@@ -26,8 +25,8 @@ inquirer
     },
     {
       type: "input",
-      message: "What is the project license?",
-      name: "license",
+      message: "What are the test instructions?",
+      name: "tests",
     },
     {
       type: "input",
@@ -36,28 +35,41 @@ inquirer
     },
     {
       type: "input",
-      message: "What are the test instructions?",
-      name: "tests",
+      message: "What is your GitHub username?",
+      name: "github",
     },
-  ])
-  .then(({ title, description, install, usage, license, contribute, tests }) => {
-    // let ghHtml = `\n`;
-    // if (!github === "") {
-    //   ghHtml = `<p>GitHub: ${github}</p>\n`;
-    // }
-    fs.writeFile(
-      "./generated.md",
-      `${buildReadme()}`,
-      (err) => (err ? console.error(err) : console.log("Success!"))
-    );
-  });
+    {
+      type: "input",
+      message: "What is your email?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is the project license?",
+      name: "license",
+    },
+  ]);
+}
 
+infoPrompt().then((answers) => {
+  fs.writeFile("./generated.md", `${buildReadme(answers)}`, (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+});
 
-
-
-function buildReadme() {
+function buildReadme({
+  title,
+  description,
+  install,
+  usage,
+  tests,
+  contribute,
+  github,
+  email,
+  license,
+}) {
   return `# ${title}
-
+${chooseBadge(license)}
   ## Description
   
   ${description}
@@ -79,19 +91,37 @@ function buildReadme() {
   
   ${usage}
   
-  ## Contributing
-  
-  ${contribute}
-  
   ## Tests
   
   ${tests}
   
+  ## Contributing
+  
+  ${contribute}
+  
   ## Questions
   
-  ${questions}
+  ${github} ${email}
   
   ## License
   
   ${license}`;
+}
+
+function chooseBadge(license) {
+  let badge;
+  if (license === "Apache 2.0") {
+    badge =
+      "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else if (license === "GNU GPL v3") {
+    badge =
+      "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (license === "MIT") {
+    badge =
+      "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "WTFPL") {
+    badge =
+      "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)";
+  }
+  return badge;
 }
